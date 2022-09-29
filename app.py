07e7b189a3cf546131d75c03c016341b6a0a52c4
stebolt <steve@stebolt.ch>
2022-09-29 17:28:48 +0200

    Added static files, template files, and API routing

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 from datetime import datetime
 import re
 
@@ -8,19 +9,16 @@ app = Flask(__name__)
 def home():
     return "Hello, Flask!"
 
+@app.route("/hello/")
+
 @app.route("/hello/<name>") # <> defines a variable after the URL that can pass to the code
 def hello_there(name):
-    now = datetime.now()
-    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+    return render_template(
+        "hello_there.html",
+        name=name,
+        date=datetime.now()
+    )
 
-    # Filter the name arg to letters only using regex. URL args
-    # can contain arbitary text, so we restrict to safe chars only.
-    match_object = re.match("[a-zA-Z]+", name)
-
-    if match_object:
-        clean_name = match_object.group(0)
-    else:
-        clean_name = "Friend"
-
-    content = "Hello there, " + clean_name + "! It's " + formatted_now
-    return content
+@app.route("/api/data")
+def get_data():
+    return app.send_static_file("data.json")
