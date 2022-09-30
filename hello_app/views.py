1dc7ff32d5ab8ce2ed9cedd22551f080ef1b0b4a
stebolt <steve@stebolt.ch>
2022-09-30 20:14:05 +0200

    Finished the VS Code Flask Tutorial with refactoring done

---
+++ hello_app/views.py
@@ -0,0 +1,32 @@
+import re
+from datetime import datetime
+
+from flask import Flask, render_template
+
+from . import app
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/hello/")
+@app.route("/hello/<name>") # <> defines a variable after the URL that can pass to the code
+def hello_there(name):
+    return render_template(
+        "hello_there.html",
+        name=name,
+        date=datetime.now()
+    )
+
+@app.route("/api/data")
+def get_data():
+    return app.send_static_file("data.json")
